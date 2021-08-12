b7d04776eb5bef3d27cf5a739c4e30a7f31fe3ef
BlackRaser <88853491+BlackRaser@users.noreply.github.com>
2021-08-13 00:17:44 +0500

    Calculator v.: 1.0
    Version: 1.0
    - Released
    - Name request
    - 2 numeric values
    - 4 mathematical actions

---
+++ Calculator v.1.0.py
@@ -0,0 +1,21 @@
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+#   Calculator v.:1.1 by BR   # 
+#/////////////////////////////#
+print('Calculator (v.:1.0) by BR')
+print('Hello!')
+print('What is your name?')
+name = input('')
+if name = ('BlackRaser') print('Hello, Father') then
+print('Nice to meet you,',name,'!')
+print('In this version, I can read simple arithmetic expressions with two variables')
+print('Now, enter the first number:')
+a = float(input())
+print('Welp. And now, enter the second number:')
+b = float(input())
+sum = (0,a+b,a-b,a*b,a/b)
+print('What are you want?')
+print('1. Summation (a+b)       3. Multple (a*b)')
+print('2. Subtract (a-b)        4. Divide (a/b)')
+print(name+', here are your numbers:',a,',',b)
+c = int(input())
+print('Total:',sum[c])
